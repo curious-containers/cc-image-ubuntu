3e0736e0b87d3385be9f076f2a66e6ffb5cd2324
Christoph Jansen <jansen.christoph@yahoo.de>
2016-08-31 11:34:24 +0200

    added --no-cache=true option for docker build to enforce cloning the latest CC-Container-Worker from github

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ REGISTRY_URL=docker.io/curiouscontainers/cc-image-ubuntu
 
 docker pull docker.io/ubuntu:xenial
 docker pull ${REGISTRY_URL}
-docker build -t ${REGISTRY_URL} .
+docker build --no-cache=true -t ${REGISTRY_URL} .
 docker push ${REGISTRY_URL}
