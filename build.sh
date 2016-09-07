083f3728924c1322d7b6e4e4887201df553ce227
Christoph Jansen <jansen.christoph@yahoo.de>
2016-09-07 11:48:47 +0200

    support git and docker tags

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 REGISTRY_URL=docker.io/curiouscontainers/cc-image-ubuntu
+TAG=
 
 docker pull docker.io/ubuntu:xenial
 docker pull ${REGISTRY_URL}
 docker build --no-cache=true -t ${REGISTRY_URL} .
 docker push ${REGISTRY_URL}
+
+if [[ ! -z ${TAG} ]]; then
+    docker tag ${REGISTRY_URL} ${REGISTRY_URL}:${TAG}
+    docker push ${REGISTRY_URL}:${TAG}
+fi
